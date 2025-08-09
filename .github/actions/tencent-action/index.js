6734c68696c76ee8cad7b3d7be8e9fe51bac7aae
taochen <15736755067@163.com>
2025-08-09 14:50:00 +0800

    update

--- .github/actions/tencent-action/index.js
+++ .github/actions/tencent-action/index.js
@@ -2,22 +2,25 @@ const tencent_cloud = require('@reverse_game/tencent_cloud')
 const core = require('@actions/core')
 async function run() {
     let action = core.getInput('action')
+
+    let result
+
     if (action === 'create_image') {
-        return await tencent_cloud.create_image(
+        result = await tencent_cloud.create_image(
             core.getInput('secret_id'),
             core.getInput('secret_key'),
             core.getInput('region'),
             core.getInput('instance_name_prefix')
         )
     } else if (action === 'get_ips') {
-        return await tencent_cloud.get_ips(
+        result = await tencent_cloud.get_ips(
             core.getInput('secret_id'),
             core.getInput('secret_key'),
             core.getInput('region'),
             core.getInput('instance_name_prefix')
         )
     } else if (action === 'create_image_sync_as') {
-        return await tencent_cloud.create_image_sync_as(
+        result = await tencent_cloud.create_image_sync_as(
             core.getInput('secret_id'),
             core.getInput('secret_key'),
             core.getInput('region'),
@@ -25,6 +28,7 @@ async function run() {
             core.getInput('launch_configuration_id')
         )
     }
+    core.setOutput('result', result)
 }
 
-await run()
+run()
