57d42b64c38909a370b088006f6f2bf8de62ee48
taochen <15736755067@163.com>
2025-08-09 22:44:26 +0800

    update

--- .github/actions/tencent-action/index.js
+++ .github/actions/tencent-action/index.js
@@ -2,33 +2,32 @@ const tencent_cloud = require('@reverse_game/tencent_cloud')
 const core = require('@actions/core')
 async function run() {
     let action = core.getInput('action')
-
-    let result
-
     if (action === 'create_image') {
-        result = await tencent_cloud.create_image(
+        let image_id = await tencent_cloud.create_image(
             core.getInput('secret_id'),
             core.getInput('secret_key'),
             core.getInput('region'),
             core.getInput('instance_name_prefix')
         )
+        core.setOutput('image_id', image_id)
     } else if (action === 'get_ips') {
-        result = await tencent_cloud.get_ips(
+        let ips = await tencent_cloud.get_ips(
             core.getInput('secret_id'),
             core.getInput('secret_key'),
             core.getInput('region'),
             core.getInput('instance_name_prefix')
         )
+        core.setOutput('ips', ips)
     } else if (action === 'create_image_sync_as') {
-        result = await tencent_cloud.create_image_sync_as(
+        let image_id = await tencent_cloud.create_image_sync_as(
             core.getInput('secret_id'),
             core.getInput('secret_key'),
             core.getInput('region'),
             core.getInput('instance_name_prefix'),
             core.getInput('launch_configuration_id')
         )
+        core.setOutput('image_id', image_id)
     }
-    core.setOutput('result', result)
 }
 
 run()
